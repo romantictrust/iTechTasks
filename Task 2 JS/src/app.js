65e599aaf7ce928e538c1d17e241e96996c66512
romantictrust <vladcirkunov598@gmail.com>
2019-08-23 19:43:29 +0300

    Added classes for Task 2

--- Task 2 JS/src/app.js
+++ Task 2 JS/src/app.js
@@ -1,2 +1,47 @@
-import './style.css';
-const APT = require ('./modules/T1_APT.js');
+import "./style.css";
+import APT from "./modules/T1_APT.js";
+
+const inp = document.querySelector(".T1_APT input");
+const btn = document.querySelector(".T1_APT button");
+const out = document.querySelector(".T1_APT .out span");
+
+let aptExpample = new APT();
+
+applyOutput(aptExpample.subSumOn([-1, 2, 3, -9]))
+
+btn.addEventListener("click", inputWorker);
+
+function getInputData() {
+  if (inp.value) return inp.value;
+}
+
+// Удаляем предыдущий output
+function cleanUp() {
+  while (out.firstChild) out.removeChild(out.firstChild);
+}
+
+// Применяем выходные данные в блок output
+function applyOutput(...outArgs) {
+  outArgs.map(element => {
+    out.appendChild(document.createTextNode(`${element}  `));
+  });
+}
+
+function getNumbersArray(str) {
+  return str.split(" ").map(Number);
+}
+
+function inputWorker() {
+  cleanUp();
+  const str = getInputData();
+  const arr = getNumbersArray(str);
+  let aptExpample = new APT();
+  const result = aptExpample.subSumOn(arr);
+  applyOutput(result);
+
+  // applyOutput(`O(n):${subSumOn(arr)}`);
+  // applyOutput(`O(n**2):${subSumOnSquared(arr)}`);
+  // applyOutput(`Min:${minElement(arr)}`);
+  // applyOutput(`Max:${maxElement(arr)}`);
+  // applyOutput(`Average:${averageElement(arr)}`);
+}
--- Task 2 JS/src/modules/T1_APT.js
+++ Task 2 JS/src/modules/T1_APT.js
@@ -1,90 +1,76 @@
-const inp = document.querySelector(".T1_APT input");
-const btn = document.querySelector(".T1_APT button");
-const out = document.querySelector(".T1_APT .out span");
-btn.addEventListener("click", APT);
+export default class ArrayProcessingTool {
+  numbersRegExp = new RegExp("^[0-9-+]+");
 
-const numbersRegExp = new RegExp("^[0-9-+]+");
+  constructor(param = '0') {
+    if (!Number(param)) return 0;
+    this.verify(param);
+    this.val = this.getNumbersArray(param);
+    this.arr = this.val;
+  }
 
-// Удаляем предыдущий output
-const cleanUp = () => {
-  while (out.firstChild) out.removeChild(out.firstChild);
-};
+  getNumbersArray (val = this.val) {
+    return val.split(" ").map(Number);
+  }
 
-// Применяем выходные данные в блок output
-const applyOutput = (...outArgs) => {
-  outArgs.map(element => {
-    out.appendChild(document.createTextNode(`${element}  `));
-  });
-};
+  verify (val = this.val) {
+    if (!this.verifier(val)) throw "Wrong string";
+  }
 
-const verifier = str => {
-  if (!numbersRegExp.test(str)) alert("Invalid String");
-  else return 1;
-};
+  verifier(str) {
+    if (!this.numbersRegExp.test(str)) alert("Invalid String");
+    else return 1;
+  }
 
-const subSumOn = arr => {
-  let cur = 0;
-  let max = 0;
-  // удаляем первые неположительные элементы массива
-  while (arr[0] <= 0) arr.splice(0, 1);
-  for (let i = 0; i < arr.length; i++) {
-    // проверяем чтобы отрицательный элемент мог дать большую сумму
-    if (arr[i] < 0 && (arr[i] + arr[i + 1] <= 0 || arr[i] + cur <= 0)) {
+  subSumOn(arr = this.arr) {
+    let cur = 0;
+    let max = 0;
+    // удаляем первые неположительные элементы массива
+    while (arr[0] <= 0) arr.splice(0, 1);
+    for (let i = 0; i < arr.length; i++) {
+      // проверяем чтобы отрицательный элемент мог дать большую сумму
+      if (arr[i] < 0 && (arr[i] + arr[i + 1] <= 0 || arr[i] + cur <= 0)) {
+        cur = 0;
+        continue;
+      }
+      cur += arr[i];
+      // получаем большую сумму из массива
+      if (cur > max) max = cur;
+    }
+    return max;
+  }
+
+  subSumOnSquared(arr = this.arr) {
+    let cur = 0;
+    let max = 0;
+    // баблсорт в мире проверки подмассива
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = i; j < arr.length; j++) {
+        cur += arr[j];
+        // Проверяем максимальную сумму в данный проход цикла.
+        if (cur < 0) continue;
+        else if (cur > max) max = cur;
+      }
       cur = 0;
-      continue;
     }
-    cur += arr[i];
-    // получаем большую сумму из массива
-    if (cur > max) max = cur;
+    return max;
   }
-  return max;
-};
 
-const subSumOnSquared = arr => {
-  let cur = 0;
-  let max = 0;
-  // баблсорт в мире проверки подмассива
-  for (var i = 0; i < arr.length; i++) {
-    for (var j = i; j < arr.length; j++) {
-      cur += arr[j];
-      // Проверяем максимальную сумму в данный проход цикла.
-      if (cur < 0) continue;
-      else if (cur > max) max = cur;
-    }
-    cur = 0;
+  minElement(arr = this.arr) {
+    const min = arr.reduce((a, b) => {
+      return a < b ? a : b;
+    });
+    return min;
   }
-  return max;
-};
 
-const minElement = arr => {
-  const min = arr.reduce((a, b) => {
-    return a < b ? a : b;
-  });
-  return min;
-};
+  maxElement(arr = this.arr) {
+    const max = arr.reduce((a, b) => {
+      return a > b ? a : b;
+    });
+    return max;
+  }
 
-const maxElement = arr => {
-  const max = arr.reduce((a, b) => {
-    return a > b ? a : b;
-  });
-  return max;
-};
-
-const averageElement = arr => {
-  const average = arr.reduce((a, b) => a + b) / arr.length;
-  return average;
-};
-
-export function APT() {
-  // проверка на числа в инпуте
-  if (!verifier(inp.value)) throw "Wrong string";
-  const val = inp.value;
-  cleanUp();
-  // выделить массив чисел
-  const arr = val.split(" ").map(Number);
-  // applyOutput(`O(n):${subSumOn(arr)}`);
-  // applyOutput(`O(n**2):${subSumOnSquared(arr)}`);
-  applyOutput(`Min:${minElement(arr)}`);
-  applyOutput(`Max:${maxElement(arr)}`);
-  applyOutput(`Average:${averageElement(arr)}`);
+  averageElement(arr = this.arr) {
+    const average = arr.reduce((a, b) => a + b) / arr.length;
+    return average;
+  }
 }
