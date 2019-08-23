390870c718dd3567a5a8b4c7d2b098e1bde73cfd
romantictrust <vladcirkunov598@gmail.com>
2019-08-23 14:34:55 +0300

    Task 2 1b added

--- Task 2 JS/src/modules/T1_APT.js
+++ Task 2 JS/src/modules/T1_APT.js
@@ -1,18 +1,30 @@
 const inp = document.querySelector(".T1_APT input");
 const btn = document.querySelector(".T1_APT button");
+const out = document.querySelector(".T1_APT .out span");
 btn.addEventListener("click", APT);
 
 const numbersRegExp = new RegExp("^[0-9-+]+");
+
+// Удаляем предыдущий output
+const cleanUp = () => {
+  while (out.firstChild) out.removeChild(out.firstChild);
+};
+
+// Применяем выходные данные в блок output
+const applyOutput = (...outArgs) => {
+  outArgs.map(element => {
+    out.appendChild(document.createTextNode(`${element}  `));
+  });
+};
 
 const verifier = str => {
   if (!numbersRegExp.test(str)) alert("Invalid String");
   else return 1;
 };
 
-const subSum = arr => {
+const subSumOn = arr => {
   let cur = 0;
   let max = 0;
-
   // удаляем первые неположительные элементы массива
   while (arr[0] <= 0) arr.splice(0, 1);
   for (let i = 0; i < arr.length; i++) {
@@ -25,15 +37,54 @@ const subSum = arr => {
     // получаем большую сумму из массива
     if (cur > max) max = cur;
   }
-  console.log(max);
   return max;
+};
+
+const subSumOnSquared = arr => {
+  let cur = 0;
+  let max = 0;
+  // баблсорт в мире проверки подмассива
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i; j < arr.length; j++) {
+      cur += arr[j];
+      // Проверяем максимальную сумму в данный проход цикла.
+      if (cur < 0) continue;
+      else if (cur > max) max = cur;
+    }
+    cur = 0;
+  }
+  return max;
+};
+
+const minElement = arr => {
+  const min = arr.reduce((a, b) => {
+    return a < b ? a : b;
+  });
+  return min;
+};
+
+const maxElement = arr => {
+  const max = arr.reduce((a, b) => {
+    return a > b ? a : b;
+  });
+  return max;
+};
+
+const averageElement = arr => {
+  const average = arr.reduce((a, b) => a + b) / arr.length;
+  return average;
 };
 
 export function APT() {
   // проверка на числа в инпуте
   if (!verifier(inp.value)) throw "Wrong string";
   const val = inp.value;
+  cleanUp();
   // выделить массив чисел
   const arr = val.split(" ").map(Number);
-  subSum(arr);
+  // applyOutput(`O(n):${subSumOn(arr)}`);
+  // applyOutput(`O(n**2):${subSumOnSquared(arr)}`);
+  applyOutput(`Min:${minElement(arr)}`);
+  applyOutput(`Max:${maxElement(arr)}`);
+  applyOutput(`Average:${averageElement(arr)}`);
 }
--- Task 2 JS/webpack.config.js
+++ Task 2 JS/webpack.config.js
@@ -1,31 +1,38 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: "./src/app.js",
   watch: true,
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     port: 3000,
     hot: true,
     watchContentBase: true,
     compress: true
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
-         rules: [
-           {
-             test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-           } 
-         ]
-       }
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
 };
