98c6279d51eae6f1b642d917216b129cf5fa5732
romantictrust <vladcirkunov598@gmail.com>
2019-09-02 12:30:28 +0300

    Processing Text Formatter

--- Task 2 JS/src/modules/T3_TF.js
+++ Task 2 JS/src/modules/T3_TF.js
@@ -9,6 +9,45 @@ export default class TextFormatter {
   }
 
   getFormattedText(str, options) {
+    let result = this.chooseType(str, options);
+    return result.join("");
+  }
 
+  chooseType(str, options) {
+    let { maxStrLen, rowsNumber, carryover } = options;
+    let strCopy = [...str];
+    if (rowsNumber) {
+      strCopy = this.formatRowsNumber(strCopy, rowsNumber);
+    }
+    if (maxStrLen) {
+      strCopy = this.formatStrLen(strCopy, maxStrLen);
+    }
+    if (carryover) {
+      strCopy = this.formatCarryover(strCopy, carryover);
+    }
+    return strCopy;
+  }
+
+  formatRowsNumber(str, rowsNumber) {
+    rowsNumber = Number(rowsNumber);
+    let shortedStrLen = Math.ceil(str.length / rowsNumber);
+    for (let i = shortedStrLen; i < str.length; i += shortedStrLen) {
+      str.splice(i, 0, "\n");
+    }
+    return str;
+  }
+
+  formatStrLen(str, maxStrLen) {
+    for (let i = maxStrLen; i < str.length; i += maxStrLen + 1) {
+      str.splice(i, 0, "\n");
+    }
+    return str;
+  }
+
+  formatCarryover(str, carryover) {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] == carryover) str.splice(i, 0, "\n"); i++;
+    }
+    return str;
   }
 }
